ca8f5f1edafe9f7d452ec78cb3e9d100f07fd228
Pranam <68323012+prnam@users.noreply.github.com>
2021-07-11 00:12:34 +0530

    correction  on day-one-arithmetic-operators.js

--- source/day-one-arithmetic-operators.js
+++ source/day-one-arithmetic-operators.js
@@ -1,3 +1,27 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
 /**
 *   Calculate the area of a rectangle.
 *
@@ -24,4 +48,12 @@ function getPerimeter(length, width) {
     let perimeter=(parseFloat(length)+parseFloat(width))*2;
     
     return perimeter;
+}
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
 }
