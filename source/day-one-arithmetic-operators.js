d151b1c2871c95453af7affb9457d683fc758d43
Pranam <68323012+prnam@users.noreply.github.com>
2021-07-11 00:01:12 +0530

    initial commit on day-one-arithmetic-operators.js

---
+++ source/day-one-arithmetic-operators.js
@@ -0,0 +1,27 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area=parseFloat(length)*parseFloat(width);
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter=(parseFloat(length)+parseFloat(width))*2;
+    
+    return perimeter;
+}
