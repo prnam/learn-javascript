ec2d39053e252b9d50c415a6120bf29db910fb63
Pranam <68323012+prnam@users.noreply.github.com>
2021-07-11 00:17:16 +0530

    correction on day-one-functions.js

--- source/day-one-functions.js
+++ source/day-one-functions.js
@@ -1,3 +1,27 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
 /*
  * Create the function factorial here
  */
@@ -7,4 +31,10 @@ function factorial(n){
     }else{
         return (n*factorial(n-1));
     }
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
 }
