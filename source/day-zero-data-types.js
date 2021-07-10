bd59d90f640dbcc09e24a350c34454fe8ea4978f
Pranam <68323012+prnam@users.noreply.github.com>
2021-07-11 00:14:42 +0530

    correction on day-zero-data-types.js

--- source/day-zero-data-types.js
+++ source/day-zero-data-types.js
@@ -1,3 +1,26 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
 
 /**
 *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
@@ -29,4 +52,12 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
    console.log(firstString+secondString) 
+}
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
 }
